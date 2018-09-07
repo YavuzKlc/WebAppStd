1d582d140d7b9b7903ef4da4ec795b52a5dbf7f9
Yavuz KILIÇ <yavuz.kilic@emse.com.tr>
2018-09-07 16:58:53 +0300

    Add files via upload

---
+++ Service/RoleService.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using Common;
+using Model;
+
+namespace Service
+{
+    public class RoleService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IGenericRepository<Role> _roleRepository;
+        private readonly IGenericRepository<User> _userRepository;
+
+        public RoleService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _roleRepository = uow.GetRepository<Role>();
+            _userRepository = uow.GetRepository<User>();
+        }
+
+        /// <summary>
+        /// Tüm roller.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<Role> GetAll()
+        {
+            return _roleRepository.GetAll();
+        }
+
+        /// <summary>
+        /// Kullanıcıya göre roller.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public IQueryable<Role> GetRolesByUser(string userName)
+        {
+            return _userRepository.GetAll().FirstOrDefault(x => x.UserName == userName).Roles.AsQueryable();
+        }
+
+        /// <summary>
+        /// Rol bul.
+        /// </summary>
+        /// <param name="roleId"></param>
+        /// <returns></returns>
+        public Role Find(int roleId)
+        {
+            return _roleRepository.Find(roleId);
+        }
+
+        /// <summary>
+        /// Kullanıcı role sahip mi.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="roleName"></param>
+        /// <returns></returns>
+        public bool IsUserInRole(string userName, string roleName)
+        {
+            return _userRepository.GetAll().FirstOrDefault(x => x.UserName == userName).Roles.Any(x => x.RoleName == roleName);
+        }
+
+        /// <summary>
+        /// Kullanıcı ekle.
+        /// </summary>
+        /// <param name="role"></param>
+        public void Insert(Role role)
+        {
+            _roleRepository.Insert(role);
+        }
+
+        /// <summary>
+        /// Kullanıcı güncelle.
+        /// </summary>
+        /// <param name="role"></param>
+        public void Update(Role role)
+        {
+            _roleRepository.Update(role);
+        }
+
+        /// <summary>
+        /// Kullanıcı sil.
+        /// </summary>
+        /// <param name="role">Rol</param>
+        public void Delete(Role role)
+        {
+            _roleRepository.Delete(role);
+        }
+
+        /// <summary>
+        /// Kullanıcı sil.
+        /// </summary>
+        /// <param name="roleId">Rol Id</param>
+        public void Delete(int roleId)
+        {
+            _roleRepository.Delete(roleId);
+        }
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+using Common;
+using Model;
+
+namespace Service
+{
+    public class UserService
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IGenericRepository<Role> _roleRepository;
+        private readonly IGenericRepository<User> _userRepository;
+
+        public UserService(IUnitOfWork uow)
+        {
+            _uow = uow;
+            _roleRepository = uow.GetRepository<Role>();
+            _userRepository = uow.GetRepository<User>();
+        }
+
+        /// <summary>
+        /// Tüm kullanıcılar.
+        /// </summary>
+        /// <returns></returns>
+        public IQueryable<User> GetAll()
+        {
+            return _userRepository.GetAll();
+        }
+
+        /// <summary>
+        /// Role göre kullanıcılar.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
+        public IQueryable<User> GetUsersByRole(string roleName)
+        {
+            return _roleRepository.GetAll().FirstOrDefault(x => x.RoleName == roleName).Users.AsQueryable();
+        }
+
+        /// <summary>
+        /// Kullanıcı sistemde kayıtlı mı.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public User ValidateUser(string userName, string password)
+        {
+            return _userRepository.GetAll().FirstOrDefault(x => x.UserName == userName && x.Password == password);
+        }
+
+        /// <summary>
+        /// Kullanıcı bul.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public User Find(int userId)
+        {
+            return _userRepository.Find(userId);
+        }
+
+        /// <summary>
+        /// Kullanıcı ekle.
+        /// </summary>
+        /// <param name="user"></param>
+        public void Insert(User user)
+        {
+            _userRepository.Insert(user);
+        }
+
+        /// <summary>
+        /// Kullanıcı güncelle.
+        /// </summary>
+        /// <param name="user"></param>
+        public void Update(User user)
+        {
+            _userRepository.Update(user);
+        }
+
+        /// <summary>
+        /// Kullanıcı sil.
+        /// </summary>
+        /// <param name="user">Kullanıcı</param>
+        public void Delete(User user)
+        {
+            _userRepository.Delete(user);
+        }
+
+        /// <summary>
+        /// Kullanıcı sil.
+        /// </summary>
+        /// <param name="userId">Kullanıcı Id</param>
+        public void Delete(int userId)
+        {
+            _userRepository.Delete(userId);
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            return _userRepository.GetAll().Any(x => x.Email == email);
+        }
+
+        public bool ValidateUserName(string userName)
+        {
+            return _userRepository.GetAll().Any(x => x.UserName == userName);
+        }
+    }
+}
